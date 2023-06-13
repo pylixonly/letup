06fbbde72c0bd36626f8abb3f7ccd7b37526f9cc
amsyarasyiq <amsyarasyiq@users.noreply.github.com>
2023-06-13 15:05:56 +0000

    deploy: c26905ff30f23a73a746ff2ffe38d2806f274892

---
+++ HoldGIFSend/index.js
@@ -0,0 +1,1 @@
+(function(t,n,r){"use strict";const e=n.findByProps("selectGIF"),c=r.instead("selectGIF",e,function(s){let[{url:u}]=s;return e.insertText(u)});return t.unLoad=c,t})({},vendetta.metro,vendetta.patcher);
