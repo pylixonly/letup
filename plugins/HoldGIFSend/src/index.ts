7d6ce4055d9965da3dd487d3e7e379e73641e974
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2023-06-13 23:09:00 +0800

    HoldGIFSend: unload properly

--- plugins/HoldGIFSend/src/index.ts
+++ plugins/HoldGIFSend/src/index.ts
@@ -3,4 +3,4 @@ import { instead } from "@vendetta/patcher";
 
 const messageInputUtil = findByProps("selectGIF");
 
-export const unLoad = instead("selectGIF", messageInputUtil, ([{ url }]) => messageInputUtil.insertText(url));
+export const onUnload = instead("selectGIF", messageInputUtil, ([{ url }]) => messageInputUtil.insertText(url));
